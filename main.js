fa6949512b6d0a85e5415aeb4007da16818a3bbd
PikapikaHei <pikachuhayden1106@gmail.com>
2026-04-25 13:50:15 +0800

    fixed bugs

--- main.js
+++ main.js
@@ -1,27 +1,31 @@
 let alpha = new Decimal(10);
 let alphaGen = new Decimal(0);
-const BuyAlphaGen = document.getElementById('BuyAlphaGen');
-const AlphaGen = document.getElementById('AlphaGen');
+
 function cost(add,mult,n,modifier,type,power) {
+    let modifierPowered = Decimal.mul(modifier, n.pow(power))
     if (type == "exponential") {
-        return add.plus(mult.mul(modifier.pow(n.pow(power))))
+        modifierPowered = modifier.pow(n.pow(power))
     };
-    if (type == "multiplicative"){
-        return add.plus(mult.mul(modifier.mul(n.pow(power))))
-    }
+    
+    //the method plus is of the Decimal namespace (idk ig cuz add isnt specified to be a Decimal type??? idk)
+    return Decimal.plus(add, (Decimal.mul(mult,(modifierPowered))));
 };
 
 function updateUI() {
     if (alphaGen != AlphaGen.textContent) {
-        AlphaGen.textContent = alphaGen
-    }
+        AlphaGen.textContent = alphaGen;
+    };
     
-} //updateUI function to be added
+}; //updateUI function to be added
 
-
-BuyAlphaGen.addEventListener('click', () => {
-    if (cost(10,1,alphaGen,2,"multiplicative",2)>alpha) {
-        alphaGen++;
-        alpha = alpha.sub(cost(10,1,alphaGen,2,"multiplicative",2));
-    }
+//make sure the website is loaded first
+document.addEventListener('DOMContentLoaded', () => {
+    const BuyAlphaGen = document.getElementById('BuyAlphaGen');
+    const AlphaGen = document.getElementById('AlphaGen');
+    BuyAlphaGen.addEventListener('click', () => {
+        if (cost(10,1,alphaGen,2,"multiplicative",2)>alpha) {
+            alphaGen++;
+            alpha = alpha.sub(cost(10,1,alphaGen,2,"multiplicative",2));
+        };
+    });
 });
