52b4b1c3ad08736577cb86ca5a8e471880597797
sdredstone <owershek2012@gmail.com>
2026-04-26 21:11:36 +0800

    GAME FUNCTIONS NOW

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
+//Non-specific
+let lastTimestamp = performance.now();
+let unlockedStage = 1 //Stage 1: Alpha, 2: Beta, 3: Gamma, etc.
+//Alpha
 let alpha = new Decimal(10);
 let alphaGen = new Decimal(0);
+let alphaGenProduction = new Decimal(1);
+
 
 function cost(add,mult,n,modifier,type,power) {
     let modifierPowered = Decimal.mul(modifier, n.pow(power))
@@ -8,24 +14,49 @@ function cost(add,mult,n,modifier,type,power) {
     };
     
     //the method plus is of the Decimal namespace (idk ig cuz add isnt specified to be a Decimal type??? idk)
-    return Decimal.plus(add, (Decimal.mul(mult,(modifierPowered))));
+    if (type == 'exponential' || type == 'multiplicative') {
+        return Decimal.plus(add, (Decimal.mul(mult,(modifierPowered))));
+    };
 };
 
 function updateUI() {
-    if (alphaGen != AlphaGen.textContent) {
-        AlphaGen.textContent = alphaGen;
+    const alphaGenTemp = alphaGen.toPrecision(3)
+    if (alphaGenTemp !== AlphaGen.textContent) {
+        AlphaGen.textContent = alphaGenTemp;
     };
-    
+    const roundedAlpha = alpha.toPrecision(3);
+    if (roundedAlpha !== Alpha.textContent) {
+        Alpha.textContent = roundedAlpha;
+    };
+    const alphaGenCostTemp = cost(10,1,alphaGen,1,"multiplicative",2).floor()
+    const alphaGenCostTemp2 = alphaGenCostTemp.toPrecision(3)
+    if (AlphaGenCost.textContent !== alphaGenCostTemp2) {
+        AlphaGenCost.textContent = alphaGenCostTemp2
+    }
 }; //updateUI function to be added
 
 //make sure the website is loaded first
 document.addEventListener('DOMContentLoaded', () => {
     const BuyAlphaGen = document.getElementById('BuyAlphaGen');
     const AlphaGen = document.getElementById('AlphaGen');
+    const Alpha = document.getElementById('Alpha')
+    const AlphaGenCost = document.getElementById('AlphaGenCost')
     BuyAlphaGen.addEventListener('click', () => {
-        if (cost(10,1,alphaGen,2,"multiplicative",2)>alpha) {
-            alphaGen++;
-            alpha = alpha.sub(cost(10,1,alphaGen,2,"multiplicative",2));
+        const costTemp = cost(10,1,alphaGen,1,"multiplicative",2).floor()
+        if (costTemp.lte(alpha)) {
+            
+            alpha = alpha.sub(costTemp);
+            alphaGen = alphaGen.plus(1);
         };
+        updateUI()
     });
+    function gameLoop(now) {
+        requestAnimationFrame(gameLoop);
+        const delta = Math.min(0.1, (now - lastTimestamp) / 1000);
+        lastTimestamp = now;
+        alpha = Decimal.add(alpha,alphaGen.times(alphaGenProduction).times(delta));
+        updateUI();
+    }   
+    gameLoop();
 });
+
